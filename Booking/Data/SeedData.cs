17f22737171e46d1068d39d380f24daae70023a8
Torbjorn Granat <torbjorn.granat@gmail.com>
2020-11-04 10:15:15 +0100

    Seed of GymClass  added. Not working yet though

---
+++ Booking/Data/SeedData.cs
@@ -0,0 +1,40 @@
+﻿using Bogus;
+using Booking.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Booking.Data
+{
+    public class SeedData
+    {
+        // Initialize database with bogus data. Called from Program.cs
+        public static async Task InitializeAsync(IServiceProvider services)
+        {
+            using (var context = new ApplicationDbContext(services.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+            {
+                if (context.GymClass.Any()) return;
+
+                var fake = new Faker("sv");
+                var gymClasses = new List<GymClass>();
+
+                for (int i = 0; i < 5; i++)
+                {
+                    var gymClass = new GymClass
+                    {
+                        Name = fake.Company.CatchPhrase(),
+                        Description = fake.Hacker.Verb(),
+                        Duration = new TimeSpan(0, 55, 0),
+                        StartDate = DateTime.Now.AddDays(fake.Random.Int(-2, 2))
+                    };
+                    gymClasses.Add(gymClass);
+                }
+
+                await context.AddRangeAsync(gymClasses);
+            }
+        }
+    }
+}
--- Booking/Program.cs
+++ Booking/Program.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Booking.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +16,35 @@ namespace Booking
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            // Seed data
+
+
+            using (var scope = host.Services.CreateScope())  // create a IServiceScope
+            {
+                var services = scope.ServiceProvider;
+                // Get database context
+                var context = services.GetRequiredService<ApplicationDbContext>();
+                // Apply pending migrations. Create database if not existing.
+                context.Database.Migrate();
+                // Get configuration (not used for now)
+                var config = services.GetRequiredService<IConfiguration>();
+
+                try
+                {
+                    SeedData.InitializeAsync(services).Wait();   // wait for task to complete
+                }
+                catch (Exception e)
+                {
+                    // Get logger. Category is class Booking.Program
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(e.Message, "Seed failed");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
