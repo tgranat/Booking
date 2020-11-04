ac291a803054d3cf469bd3958a6c81e2fc0f42a4
Torbjorn Granat <torbjorn.granat@gmail.com>
2020-11-04 09:11:05 +0100

    Scaffolding Identity

---
+++ Booking/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Booking.Data;
+using Booking.Models.Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Booking.Areas.Identity.IdentityHostingStartup))]
+namespace Booking.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
