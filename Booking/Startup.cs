9c5407f35c48ebf95834afad6190831032d7b8cf
Torbjorn Granat <torbjorn.granat@gmail.com>
2020-11-04 10:48:37 +0100

    Add SQL logging

--- Booking/Startup.cs
+++ Booking/Startup.cs
@@ -30,7 +30,7 @@ namespace Booking
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString("DefaultConnection")).EnableSensitiveDataLogging());
 
             // Default IdentityUser changed to ApplicationUser (which extends IdentityUser)
             services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false) 
