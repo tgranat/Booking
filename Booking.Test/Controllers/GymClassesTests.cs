50ab0d16ed466ee67f6d28183767cea14e7f6313
Torbjorn Granat <torbjorn.granat@gmail.com>
2020-11-10 09:33:57 +0100

    Added tests

--- Booking.Test/Controllers/GymClassesTests.cs
+++ Booking.Test/Controllers/GymClassesTests.cs
@@ -5,12 +5,14 @@ using Booking.Repositories;
 using Booking.Test.Extensions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Booking.Test.Controllers
 {
@@ -54,13 +56,31 @@ namespace Booking.Test.Controllers
                      Duration = g.Duration
                  });
 
-            controller.SetUserIsAuthenticated(false);
+            controller.SetUserIsAuthenticated(false); // Extension 
             repository.Setup(r => r.GetAsync()).ReturnsAsync(gymClasses);
             var viewModel = new IndexViewModel { ShowHistory = false };
             var viewResult = controller.Index(viewModel).Result as ViewResult;
             var actual = viewResult.Model as IndexViewModel;
             Assert.AreEqual(expected.GymClasses.Count(), actual.GymClasses.Count());
         }
+
+        [TestMethod]
+        public async Task Index_AuthenticatedReturnsViewResult_ShouldPass()
+        {
+            controller.SetUserIsAuthenticated(true);
+            var viewModel = new IndexViewModel { ShowHistory = false };
+            var result = await controller.Index(viewModel);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        // Should return null since view not set yet (view for Create (found in Booking/Views/GymClasses) is bound after)
+        [TestMethod]
+        public void Create_ReturnsDefaultView_ShoudReturnNull()
+        {
+            var result = controller.Create() as ViewResult;
+            Assert.IsNull(result.ViewName);
+        }
+
 
         private List<GymClass> GetGymClassList()
         {
